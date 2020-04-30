ac3200cc443aea99d79199cc2f8445cab9720cd3
Nancy <gnancy417@gmail.com>
2020-05-01 01:28:25 +1000

    update amazon_s3_cli.py

--- ACNH_API/amazon_s3_cli.py
+++ ACNH_API/amazon_s3_cli.py
@@ -13,6 +13,41 @@ import os
 env_path = Path('..') / '.env'
 load_dotenv(dotenv_path=env_path)
 
+# import watchdog libs
+import time
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+
+def onEvent(event):
+    if event.event_type in ['created', 'deleted', 'move']:
+        # Take any action here when a file is first created.
+        sync_data()
+        print("{} - {}.".format(event.event_type, event.src_path))
+
+def createEventHandler():
+    patterns = "*"
+    ignore_patterns = ""
+    ignore_directories = False
+    case_sensitive = True
+    my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+    my_event_handler.on_moved = onEvent
+    my_event_handler.on_created = onEvent
+    my_event_handler.on_deleted = onEvent
+
+    return my_event_handler
+
+def folder_watcher(local_folder,my_event_handler):
+    go_recursively = True
+    my_observer = Observer()
+    my_observer.schedule(my_event_handler, local_folder, recursive=go_recursively)
+
+    my_observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        my_observer.stop()
+        my_observer.join()
 
 def sync_data():
     local_folder = os.getenv('LocalFolder')
@@ -26,4 +61,6 @@ def sync_data():
 
 
 if __name__ == '__main__':
-    sync_data()
+    my_event_handler = createEventHandler()
+    folder_watcher('data', my_event_handler)
+
